c82911b38bdd722baa6cd854097464121691639a
Fred Zhen <13911239075@163.com>
2021-11-15 00:46:12 +0800

    Added Visit class, updated Owner class with Pet,  Closes #28

--- pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.ysy.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by zhenrui on 2021/11/1 21:40
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
