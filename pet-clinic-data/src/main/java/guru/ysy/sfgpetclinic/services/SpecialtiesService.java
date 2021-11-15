d020321511d5da5802d843c6913a1ca8f3847d7a
Fred Zhen <13911239075@163.com>
2021-11-15 13:19:50 +0800

    Added Specialty Map Service Implementation

---
+++ pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package guru.ysy.sfgpetclinic.services;
+
+import guru.ysy.sfgpetclinic.model.Specialty;
+
+/**
+ * Created by zhenrui on 2021/11/15 13:11
+ */
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
---
+++ pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,38 @@
+package guru.ysy.sfgpetclinic.services.map;
+
+import guru.ysy.sfgpetclinic.model.Specialty;
+import guru.ysy.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by zhenrui on 2021/11/15 13:13
+ */
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
