365d3bd3ed92b3bab50aeb34e4ac6a149936211c
Fred Zhen <13911239075@163.com>
2021-11-15 13:07:44 +0800

    Rebased with all the new classes and interfaces so far

--- pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/model/Vet.java
@@ -1,12 +1,13 @@
 package guru.ysy.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by zhenrui on 2021/11/1 21:39
  */
 public class Vet extends Person {
-    private Set<Specialty> specialties;
+    private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
         return specialties;
--- pet-clinic-web/src/main/java/guru/ysy/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/ysy/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,16 @@
 package guru.ysy.sfgpetclinic.bootstrap;
 
 import guru.ysy.sfgpetclinic.model.Owner;
+import guru.ysy.sfgpetclinic.model.Pet;
+import guru.ysy.sfgpetclinic.model.PetType;
 import guru.ysy.sfgpetclinic.model.Vet;
 import guru.ysy.sfgpetclinic.services.OwnerService;
+import guru.ysy.sfgpetclinic.services.PetTypeService;
 import guru.ysy.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 /**
  * Created by zhenrui on 2021/11/9 21:05
@@ -15,23 +20,54 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Dog");
+        PetType savedCatType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brick road");
+        owner1.setCity("Miami");
+        owner1.setTelephone("12311123564");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner1.setAddress("567 Culling road");
+        owner1.setCity("New York");
+        owner1.setTelephone("13945678912");
+
+        Pet fionaPet = new Pet();
+        fionaPet.setPetType(savedCatType);
+        fionaPet.setOwner(owner2);
+        fionaPet.setBirthDate(LocalDate.now());
+        fionaPet.setName("Mime");
+        owner2.getPets().add(fionaPet);
 
         ownerService.save(owner2);
 
