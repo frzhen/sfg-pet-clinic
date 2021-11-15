2413886919958090c18238134089b02e98fdb462
Fred Zhen <13911239075@163.com>
2021-11-15 12:17:50 +0800

    Added PetTypeService, and PetTypeServiceMap that implements PetTypeService,  Closes #31

---
+++ pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.ysy.sfgpetclinic.services;
+
+import guru.ysy.sfgpetclinic.model.PetType;
+
+/**
+ * Created by zhenrui on 2021/11/15 11:59
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
---
+++ pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,38 @@
+package guru.ysy.sfgpetclinic.services.map;
+
+
+import guru.ysy.sfgpetclinic.model.PetType;
+import guru.ysy.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+/**
+ * Created by zhenrui on 2021/11/15 11:57
+ */
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
