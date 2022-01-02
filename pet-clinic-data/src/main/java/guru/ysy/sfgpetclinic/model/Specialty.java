5d86157000499a7f6286a82736f5451cc0afe0c9
Fred Zhen <13911239075@163.com>
2022-01-02 12:41:19 +0800

    Implemented Vet JPA, including Specialty class, Closes #39

--- pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/model/Specialty.java
@@ -1,10 +1,17 @@
 package guru.ysy.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by zhenrui on 2021/11/15 11:13
  */
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
