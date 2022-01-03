8c7e3ba1c73c5c1bcea632325ad57a07d12e3203
Fred Zhen <13911239075@163.com>
2022-01-03 12:29:37 +0800

    Added Spring Data JPA Service for Owners, Closes #42

---
+++ pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,67 @@
+package guru.ysy.sfgpetclinic.services.springdatajpa;
+
+import guru.ysy.sfgpetclinic.model.Owner;
+import guru.ysy.sfgpetclinic.repositories.OwnerRepository;
+import guru.ysy.sfgpetclinic.repositories.PetRepository;
+import guru.ysy.sfgpetclinic.repositories.PetTypeRepository;
+import guru.ysy.sfgpetclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by zhenrui on 2022/1/3 11:55
+ */
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+                             PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+
+
+    @Override
+    public Set<Owner> findAll() {
+
+        Set<Owner> owners = new HashSet<>();
+
+        ownerRepository.findAll().forEach(owners::add);
+
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
