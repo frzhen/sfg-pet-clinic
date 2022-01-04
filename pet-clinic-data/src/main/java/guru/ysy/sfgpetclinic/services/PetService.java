70f6c038a85eba366d64736b735713911914fd30
Fred Zhen <13911239075@163.com>
2022-01-04 23:01:16 +0800

    Added Spring Data JPA Service for Pet, Closes #47

--- pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/services/PetService.java
@@ -1,14 +1,10 @@
 package guru.ysy.sfgpetclinic.services;
 
-import guru.ysy.sfgpetclinic.model.Owner;
 import guru.ysy.sfgpetclinic.model.Pet;
 
 /**
  * Created by zhenrui on 2021/11/3 21:07
  */
 public interface PetService extends CrudService<Pet, Long>{
-
-    Owner findByLastName(String lastName);
-
 
 }
---
+++ pet-clinic-data/src/main/java/guru/ysy/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,51 @@
+package guru.ysy.sfgpetclinic.services.springdatajpa;
+
+import guru.ysy.sfgpetclinic.model.Pet;
+import guru.ysy.sfgpetclinic.repositories.PetRepository;
+import guru.ysy.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by zhenrui on 2022/1/4 22:54
+ */
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
